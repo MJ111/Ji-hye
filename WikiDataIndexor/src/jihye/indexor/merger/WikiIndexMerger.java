e596aebf62c607630e22876a1da95114b1932c5e
mell03 <mell03@naver.com>
2015-05-10 14:53:10 +0900

    Merger 만드는중

--- WikiDataIndexor/src/jihye/indexor/merger/WikiIndexMerger.java
+++ WikiDataIndexor/src/jihye/indexor/merger/WikiIndexMerger.java
@@ -16,6 +16,8 @@ import java.util.SortedSet;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
+import javax.swing.text.html.HTMLDocument.Iterator;
+
 import jihye.indexor.util.Utility;
 
 
@@ -30,15 +32,89 @@ public class WikiIndexMerger {
 	}
 	
 	public void startMerge(int seperate) {
-		index = getIndexOfIndices(seperate);
+		//index = getIndexOfIndices(seperate);
+		index = readIndexOfIndicesFromFile();
+		iNumOfIndex = index.size() / seperate + 1;
 		merge();
 	}
 	
 	private void merge() {
+		ArrayList<HashMap<String, SortedSet<Long>>> ret = new ArrayList<HashMap<String,SortedSet<Long>>>();
 		//Make files
 		File[] files = Utility.getInstance().createFiles(dictionaryPath, iNumOfIndex, Utility.FILE_TYPE_INDEX);
+		System.out.println("Found " + matchingFiles.length + " to be merged");
+		
+		for(File f : matchingFiles) {
+			try {
+				FileInputStream fis = new FileInputStream(f);
+				ObjectInputStream ois = new ObjectInputStream(fis);
+				ret.add((HashMap<String, SortedSet<Long>>)ois.readObject());
+				ois.close();
+				fis.close();
+			} catch(Exception e){
+				e.printStackTrace();
+			}
+		}
 		
 		
+		Set<String> keys = index.keySet();			
+		
+		for(int i = 0; i < iNumOfIndex;) {
+			System.out.println("merging :" + i);
+			SortedMap<String, SortedSet<Long>> nIndex = new TreeMap<String, SortedSet<Long>>();	
+			
+			
+			Set<String> nKeys = new TreeSet<String>();
+			for(String key : keys) {
+				if(index.get(key) == i) {
+					nKeys.add(key);
+				}
+			}
+			
+			for(String key : nKeys) {
+				SortedSet<Long> set = new TreeSet<Long>();
+				nIndex.put(key, set);
+				
+				for(HashMap<String, SortedSet<Long>> map : ret) {
+					if(map.containsKey(key)) {
+						set.addAll(map.get(key));
+					}
+				}
+			}
+			
+			try {				
+				
+				FileOutputStream fos = new FileOutputStream(files[i]);
+				ObjectOutputStream oos = new ObjectOutputStream(fos);
+				oos.writeObject(null);
+				fos.close();
+				oos.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+		}	
+	}
+	
+	private SortedMap<String, Integer> readIndexOfIndicesFromFile() {
+		SortedMap<String, Integer> ret = null;
+		
+		File[] f = Utility.getInstance().getFiles(dictionaryPath, "jhdindex");
+		if(f.length != 0) {
+			System.out.println("Load Data From :" + f[0]);
+			try {
+				FileInputStream fis = new FileInputStream(f[0]);
+				ObjectInputStream ois = new ObjectInputStream(fis);
+				ret = (SortedMap<String, Integer>)ois.readObject();
+				fis.close();
+				ois.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}else {
+			System.out.println("Index of Index Data Not Found : " + dictionaryPath);
+		}
+		return ret;		
 	}
 	
 	private SortedMap<String, Integer> getIndexOfIndices(int seperate) {
