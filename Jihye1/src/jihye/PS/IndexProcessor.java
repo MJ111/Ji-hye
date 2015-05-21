d96458100b511f7b34e3b0dc75875e9d339ddc14
mell03 <mell03@naver.com>
2015-05-21 12:26:55 +0900

    Index 불러오는거까지 했음

--- Jihye1/src/jihye/PS/IndexProcessor.java
+++ Jihye1/src/jihye/PS/IndexProcessor.java
@@ -14,8 +14,9 @@ import java.util.TreeMap;
 
 import org.apache.commons.lang3.ArrayUtils;
 
+
 public class IndexProcessor {
-	Map<String, int[]> indices = null;
+	Map<String, Triple<Float, int[], float[]>> indices = null;
 	
 	public IndexProcessor(String indexPath) throws Exception{
 		loadIndex(indexPath);
@@ -23,26 +24,31 @@ public class IndexProcessor {
 	
 	private void loadIndex(String indexPath) throws IOException{
 		if(indices != null) return;
-		indices = new TreeMap<String, int[]>();
+		indices = new TreeMap<String, Triple<Float, int[], float[]>>();
 		//TODO : Something wrong with this... 파일을 메인에서 받아야할텐데..
 		File file = new File(indexPath);
 		
 		FileInputStream fis = new FileInputStream(file);
 		InputStreamReader isr = new InputStreamReader(fis,"UTF-8");
-		BufferedReader br = new BufferedReader(isr);	
+		BufferedReader br = new BufferedReader(isr);
 		
 		while(br.ready()) {
 			String line = br.readLine();
 			String[] seperated = line.split(",");
-			int postingsSize = seperated.length - 1;
+			int postingsSize = (seperated.length - 1)/2;
 			
 			String term = seperated[0];
+			float idf = Float.parseFloat(seperated[1]);
 			int[] postings = new int[postingsSize];
+			float[] tfidfs = new float[postingsSize];
 			
-			for(int i = 0; i < postingsSize; i++) {
-				postings[i] = Integer.parseInt(seperated[i+1]);
+			for(int i = 2; i < postingsSize; i=i+2) {
+				postings[i/2] = Integer.parseInt(seperated[i]);
+				tfidfs[i/2] = Float.parseFloat(seperated[i+1]);
 			}			
-			indices.put(term, postings);			
+			
+			Triple<Float, int[], float[]> posting = new Triple<Float, int[], float[]>(idf, postings, tfidfs);
+			indices.put(term, posting);			
 		}		
 		br.close();
 		isr.close();
@@ -50,16 +56,17 @@ public class IndexProcessor {
 	}
 	
 	public int[] getPostings(String term) {
-		return indices.get(term);
+		return null;
+				//indices.get(term);
 	}
 	
 	public int[] getMergedPostings(List<String> terms, float rate) {
 		List<int[]> postings = new ArrayList<int[]>();		
 		
-		for(String term : terms) {
-			if(indices.containsKey(term))
-				postings.add(indices.get(term));
-		}
+//		for(String term : terms) {
+//			if(indices.containsKey(term))
+//				postings.add(indices.get(term));
+//		}
 		
 		return mergePostings(postings, rate);
 	}
